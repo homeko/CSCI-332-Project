dde1809fcd3b1fa281242c1e5e9e9519bdb4c7f7
Kevin <kossa526@gmail.com>
2018-02-21 08:57:27 -0500

    minor fix

--- project/login.php
+++ project/login.php
@@ -5,10 +5,10 @@
   if($_SERVER["REQUEST_METHOD"] == "POST") {
     // username and pass sent to form
 
-    $myuser = mysqli_real_escape_string($db,$_POST['username']);
-    $mypass = mysqli_real_escape_string($db,$_POST['password']);
+    $myusername = mysqli_real_escape_string($db,$_POST['username']);
+    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-    $sql = "SELECT id FROM Admin WHERE username = '$myuser' AND password = '$mypass'";
+    $sql = "SELECT id FROM Admin WHERE username = '$myusername' AND password = '$mypassword'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
@@ -20,7 +20,6 @@
     if($count == 1) {
       session_register("myusername");
       $_SESSION['login_user'] = $myuser;
-
 
       header("location: welcome.php");
     }
@@ -55,8 +54,8 @@
             <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
             <div style = "margin:30px">
                <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <label>Username: </label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password: </label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
